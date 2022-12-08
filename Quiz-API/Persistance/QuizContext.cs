ef8e0175b8e9284234a2c20eccc2d291dd530d1d
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-08 12:08:11 +0100

    AnswerAlternative

--- Quiz-API/Persistance/QuizContext.cs
+++ Quiz-API/Persistance/QuizContext.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Quiz_API.Models;
+
+
 namespace Quiz_API.Persistance
 {
-	public class QuizContext
-	{
-		public QuizContext()
+	public class QuizContext : DbContext
+    {
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<AnswerAlternative> AnswerAlternatives { get; set; }
+
+        public QuizContext()
 		{
 		}
 	}

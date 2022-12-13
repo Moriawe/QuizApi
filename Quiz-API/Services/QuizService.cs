cd3abdcf61ebf57ff802b1ecb3361b3db56220de
Jennie <91130763+Moriawe@users.noreply.github.com>
2022-12-13 08:51:33 +0100

    last changes on Mac

--- Quiz-API/Services/QuizService.cs
+++ Quiz-API/Services/QuizService.cs
@@ -1,3 +1,5 @@
+using Quiz_API.Models;
+
 namespace Quiz_API.Services;
 
 // Skall bli en stor mastodont fil som hanterar all logic i hela applikationen
@@ -7,9 +9,34 @@ public class QuizService
     {
         
     }
-
-    public GetQuiz()
+    
+    // Slumpa vilken väg den skall hämta en quiz
+    public QuizModel GetQuiz()
     {
+        return (quizModel);
+    }
 
+    // Hämta en quiz som innehåller 1 fråga och 4 svarsalternativ från QuizAdapter
+    public QuizModel GetDBQuiz()
+    {
+        return (quizModel);
+    }
+    
+    // Hämta en quiz som innehåller 1 fråga och 4 svarsalternativ från TriviaAdapter
+    public QuizModel GetTriviaQuiz()
+    {
+        return (quizModel);
+    }
+    
+    // Kolla om quizzen redan finns i databasen
+    public checkIfQuizExists()
+    {
+        
+    }
+    
+    // Lägg till quizzen i databasen
+    public AddQuizToDatabase()
+    {
+        
     }
 }

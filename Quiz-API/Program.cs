54ba7c997e867743a4b3445c8957c25a29347703
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-13 19:57:35 +0100

    Dependency Injection for the Questions flow.

--- Quiz-API/Program.cs
+++ Quiz-API/Program.cs
@@ -1,4 +1,7 @@
-﻿using Quiz_API.Persistance;
+﻿using Quiz_API.Adapters;
+using Quiz_API.Persistance;
+using Quiz_API.Repositories;
+using Quiz_API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Later: <Interface, ImplementingClass>
 builder.Services.AddScoped<QuizDatabaseContext, QuizDatabaseContext>();
+builder.Services.AddScoped<QuestionRepository, QuestionRepository>();
+builder.Services.AddScoped<QuestionAdapter, QuestionAdapter>();
+builder.Services.AddScoped<QuestionService, QuestionService>();
+
+
+
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle

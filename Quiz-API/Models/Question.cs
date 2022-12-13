1c8c75412c57e0b35c63bba3794f0dd16c8c1790
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-13 12:20:22 +0100

    Question

--- Quiz-API/Models/Question.cs
+++ Quiz-API/Models/Question.cs
@@ -4,16 +4,14 @@ namespace Quiz_API.Models
     public class Question
     {
         public Guid Id { get; set; }
-        public string Language { get; set; } = "svenska";
         public string Text { get; set; }
         public string Category { get; set; } // music, history, film
 
         //public virtual IList<Answer> Answers { get; set; }
 
-        public Question(string language, string text, string category)
+        public Question(string text, string category)
         {
             this.Id = Guid.NewGuid(); // Let database do this (but sqlite generates Id:s as int:s...)
-            this.Language = language;
             this.Text = text;
             this.Category = category;
 

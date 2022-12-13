92eda777b266df2dcb7fc7f971f77de57a0a78c8
Moriawe <moriawe22@gmail.com>
2022-12-13 19:59:42 +0100

    add a TriviaRepository and Adapter and replaced correct/incorrect answers for a List of Answers

--- Quiz-API/Adapters/TriviaAdapter.cs
+++ Quiz-API/Adapters/TriviaAdapter.cs
@@ -1,7 +1,39 @@
+using Quiz_API.Models;
+using Quiz_API.Repositories;
+
 namespace Quiz_API.Adapters;
 
 // Skall hantera kommunikationen med Trivia repot.
-public class TriviaAdapter
+/*public class TriviaAdapter
 {
-    
-}
+    private TriviaRepository _triviaRepository;
+
+    private TriviaModel triviaModel;
+    private QuizModel? quizmodel;
+    public TriviaAdapter()
+    {
+        _triviaRepository = new TriviaRepository();
+    }
+
+    public async Task<QuizModel> GetOneTriviaQuiz()
+    {
+        var response = await _triviaRepository.GetTriviaQuiz();
+        Console.WriteLine(response);
+        QuizModel responseQuiz;
+        
+         foreach (TriviaModel quiz in response)
+         {  
+             List<Answer> Answers = new List<Answer>();
+             Answers.Add(quiz.CorrectAnswer, quiz.Id, true);
+             
+             foreach (string answer in quiz.IncorrectAnswers)
+             {
+                 Answers.Add(new Answer(answer, quiz.Id, false));
+             }
+
+             responseQuiz = new QuizModel(quiz.Category, Answers, quiz.Question);
+         }
+
+         return responseQuiz;
+    }
+}*/
--- Quiz-API/Models/QuizModel.cs
+++ Quiz-API/Models/QuizModel.cs
@@ -5,16 +5,13 @@ public class QuizModel
 {
     public Guid Id { get; set; }
     public string Category { get; set; }
-    // Skall rätt svar också vara en lista? ifall det finns flera rätt svar?
-    public Answer CorrectAnswer { get; set; }
     public List<Answer> IncorrectAnswers { get; set; }
     public string Question { get; set; }
 
-    public QuizModel(string category, Answer correctAnswer, List<Answer> incorrectAnswers, string question)
+    public QuizModel(string category, List<Answer> incorrectAnswers, string question)
     {
         Id = Guid.NewGuid();
         Category = category;
-        CorrectAnswer = correctAnswer;
         IncorrectAnswers = incorrectAnswers;
         Question = question;
     }

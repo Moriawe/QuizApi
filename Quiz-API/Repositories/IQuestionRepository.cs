5a3f62219e76756d380c81653a75343de1eea245
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-14 14:02:03 +0100

    IQuestionRepository

---
+++ Quiz-API/Repositories/IQuestionRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Quiz_API.Models;
+
+namespace Quiz_API
+{
+	public interface IQuestionRepository
+	{
+        public List<Question> Get();
+        public Question? Get(Guid Id);
+        public Question Post(Question question);
+        public Question? Put(Question question);
+        public bool Delete(Question question);
+    }
+}
+

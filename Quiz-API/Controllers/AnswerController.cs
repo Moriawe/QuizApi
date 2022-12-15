0716d325e92a50d2fda40c5b748ce8b50b3f4d42
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-15 12:00:14 +0100

    [ApiController] and ControllerBase

--- Quiz-API/Controllers/AnswerController.cs
+++ Quiz-API/Controllers/AnswerController.cs
@@ -7,90 +7,91 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace Quiz_API.Controllers;
 
-    [Route("api/[controller]")]
-    [Produces(MediaTypeNames.Application.Json)]
-    public class AnswerController : Controller
+[ApiController]
+[Route("api/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class AnswerController : ControllerBase
+{
+    private AnswerService _answerService;
+
+    public AnswerController(AnswerService answerService)
     {
-        private AnswerService _answerService;
-        
-        public AnswerController(AnswerService answerService)
-        {
         _answerService = answerService;
-        }
-        
-        //TODO Behöver vi hämta alla svar? Känns mer logiskt att hämta ALLA frågor
-        // GET: api/values
-        [HttpGet]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
-        public IActionResult Get()
-        {
-            return Ok(_answerService.GetAllAnswers());
-        }
-
-     
-        // GET api/values/5
-        [HttpGet("{id}")]
-        [SwaggerResponse((int)HttpStatusCode.NotFound)]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Answer))]
-        public IActionResult Get(Guid id)
-        {
-           return Ok(_answerService.GetAnswers(id));
-        }
-
-        // ANVÄNDA ASYNC OCH AWAIT?
-        /*
-        public async Task<ActionResult<Answer>> GetAnswer(string id)
-        {
-            var answers = await _answerService.GetAnswers(id);
-            if (answers == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(answers);
-        }
-        */
-
-        // POST api/values
-        [HttpPost]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
-        public IActionResult Post([FromBody] Answer answer)
-        {
-            return Ok(_answerService.PostAnswer(answer));
-        }
-        
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        [SwaggerResponse((int)HttpStatusCode.NotFound)]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
-        public IActionResult Put(Guid id, [FromBody] Answer answer)
-        {
-            if (id != answer.Id)
-            {
-                return BadRequest("Invalid id");
-            }
-            
-            var isAnswerUpdated = _answerService.PutAnswer(answer);
-            if (isAnswerUpdated)
-            {
-                return Ok();
-            }
-            return NotFound();
-            
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        [SwaggerResponse((int)HttpStatusCode.NotFound)]
-        [SwaggerResponse((int)HttpStatusCode.NoContent)]
-        public IActionResult Delete(Guid id)
-        {
-            var isAnswerDeleted = _answerService.DeleteAnswer(id);
-            if (isAnswerDeleted)
-            {
-                return Ok();
-            }
-
-            return NotFound();
-        }
     }
+
+    //TODO Behöver vi hämta alla svar? Känns mer logiskt att hämta ALLA frågor
+    // GET: api/values
+    [HttpGet]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
+    public IActionResult Get()
+    {
+        return Ok(_answerService.GetAllAnswers());
+    }
+
+
+    // GET api/values/5
+    [HttpGet("{id}")]
+    [SwaggerResponse((int)HttpStatusCode.NotFound)]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Answer))]
+    public IActionResult Get(Guid id)
+    {
+        return Ok(_answerService.GetAnswers(id));
+    }
+
+    // ANVÄNDA ASYNC OCH AWAIT?
+    /*
+    public async Task<ActionResult<Answer>> GetAnswer(string id)
+    {
+        var answers = await _answerService.GetAnswers(id);
+        if (answers == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(answers);
+    }
+    */
+
+    // POST api/values
+    [HttpPost]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
+    public IActionResult Post([FromBody] Answer answer)
+    {
+        return Ok(_answerService.PostAnswer(answer));
+    }
+
+    // PUT api/values/5
+    [HttpPut("{id}")]
+    [SwaggerResponse((int)HttpStatusCode.NotFound)]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
+    public IActionResult Put(Guid id, [FromBody] Answer answer)
+    {
+        if (id != answer.Id)
+        {
+            return BadRequest("Invalid id");
+        }
+
+        var isAnswerUpdated = _answerService.PutAnswer(answer);
+        if (isAnswerUpdated)
+        {
+            return Ok();
+        }
+        return NotFound();
+
+    }
+
+    // DELETE api/values/5
+    [HttpDelete("{id}")]
+    [SwaggerResponse((int)HttpStatusCode.NotFound)]
+    [SwaggerResponse((int)HttpStatusCode.NoContent)]
+    public IActionResult Delete(Guid id)
+    {
+        var isAnswerDeleted = _answerService.DeleteAnswer(id);
+        if (isAnswerDeleted)
+        {
+            return Ok();
+        }
+
+        return NotFound();
+    }
+}
--- Quiz-API/Controllers/QuizController.cs
+++ Quiz-API/Controllers/QuizController.cs
@@ -7,48 +7,49 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace Quiz_API.Controllers;
 
-    [Route("api/[controller]")]
-    [Produces(MediaTypeNames.Application.Json)]
-    public class QuizController : Controller
-    {
-        private QuizService _quizService;
-        
-        public QuizController(QuizService quizService)
-        {
-        _quizService = quizService;
-        }
-        
-        // GET: api/values
-        [HttpGet("Database")]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizModel))]
-         public IActionResult GetDatabase()
-        {
-            return Ok(_quizService.GetOneDbQuiz());
-        } 
-        
-        // GET: api/values
-        [HttpGet("Trivia")]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizModel))]
-        public async Task<IActionResult> GetTrivia()
-        {
-            return Ok(await _quizService.GetTriviaQuiz());
-        }
-        
-        // POST api/values
-        [HttpPost]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<QuizModel>))]
-        public IActionResult Post([FromBody] QuizModel quizModel)
-        {
-            return Ok(_quizService.AddQuizToDatabase(quizModel));
-        }
-     
-        // // GET api/values/5
-        // [HttpGet("{limit}")]
-        // [SwaggerResponse((int)HttpStatusCode.NotFound)]
-        // [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Answer))]
-        // public IActionResult Get(int limit)
-        // {
-        //    return Ok(_quizService);
-        // }
+[ApiController]
+[Route("api/[controller]")]
+[Produces(MediaTypeNames.Application.Json)]
+public class QuizController : ControllerBase
+{
+    private QuizService _quizService;
 
+    public QuizController(QuizService quizService)
+    {
+        _quizService = quizService;
     }
+
+    // GET: api/values
+    [HttpGet("Database")]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizModel))]
+    public IActionResult GetDatabase()
+    {
+        return Ok(_quizService.GetOneDbQuiz());
+    }
+
+    // GET: api/values
+    [HttpGet("Trivia")]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizModel))]
+    public async Task<IActionResult> GetTrivia()
+    {
+        return Ok(await _quizService.GetTriviaQuiz());
+    }
+
+    // POST api/values
+    [HttpPost]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<QuizModel>))]
+    public IActionResult Post([FromBody] QuizModel quizModel)
+    {
+        return Ok(_quizService.AddQuizToDatabase(quizModel));
+    }
+
+    // // GET api/values/5
+    // [HttpGet("{limit}")]
+    // [SwaggerResponse((int)HttpStatusCode.NotFound)]
+    // [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Answer))]
+    // public IActionResult Get(int limit)
+    // {
+    //    return Ok(_quizService);
+    // }
+
+}

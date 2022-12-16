cdd71892f8ff7b3a7b6bdab422d457b42871ab02
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-16 16:19:28 +0100

    Swagger also in production (not just debug).

--- Quiz-API/Program.cs
+++ Quiz-API/Program.cs
@@ -43,11 +43,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 

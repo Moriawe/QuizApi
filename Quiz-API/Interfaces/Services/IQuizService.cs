53ceb8208da5d61ea1a323c487797bff3ee6e3a4
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-16 14:29:38 +0100

    IQuizService

--- Quiz-API/Interfaces/Services/IQuizService.cs
+++ Quiz-API/Interfaces/Services/IQuizService.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using Quiz_API.Adapters;
+using Quiz_API.Models;
+
 namespace Quiz_API
 {
-	public class IQuizService
+	public interface IQuizService
 	{
 		public IQuizService()
 		{
 		}
-	}
+
+
+        public QuizModel GetDbQuiz(Guid id);
+        public QuizModel GetOneDbQuiz();
+        public Task<QuizModel> GetTriviaQuiz();
+        public bool isAnswerCorrect(Answer answer);
+        public bool checkIfQuizExistsinDb(Guid id);
+        public QuizModel AddQuizToDatabase(QuizModel quiz);
+        public void UpdateQuizInDatabase(QuizModel quiz);
+        public void DeleteQuizFromDatabase(QuizModel quiz);
+    }
 }
 

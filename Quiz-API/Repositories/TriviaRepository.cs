0a50aa4106ac82f685ed611c19853b605a03029b
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-19 15:05:33 +0100

    6

--- Quiz-API/Repositories/TriviaRepository.cs
+++ Quiz-API/Repositories/TriviaRepository.cs
@@ -1,26 +1,27 @@
 using System.Text.Json;
 using Quiz_API.Models;
 
-namespace Quiz_API.Repositories;
-
-// Skall hämta information ifrån Trivia API:et
-public class TriviaRepository
+namespace Quiz_API.Repositories
 {
-    HttpClient _client = new();
-    
-    public async Task<List<TriviaModel>> GetTriviaAsync()
+
+    // Skall hämta information ifrån Trivia API:et
+    public class TriviaRepository
     {
-        var uri = $"https://the-trivia-api.com/api/questions?limit=1";
-        List<TriviaModel> triviaQuizzes = new();
-        
-        // Skillnad på stream, get StreamAsync och just GetAsync
-        //await using Stream stream = await _client.GetStreamAsync(uri);
-        var response = await _client.GetAsync(uri);
-        var stream = await response.Content.ReadAsStreamAsync();
-        
-        triviaQuizzes = await JsonSerializer.DeserializeAsync<List<TriviaModel>>(stream);
-        Console.WriteLine(triviaQuizzes);
-        return triviaQuizzes;
+        HttpClient _client = new();
+
+        public async Task<List<TriviaModel>> GetTriviaAsync()
+        {
+            var uri = $"https://the-trivia-api.com/api/questions?limit=1";
+            List<TriviaModel> triviaQuizzes = new();
+
+            // Skillnad på stream, get StreamAsync och just GetAsync
+            //await using Stream stream = await _client.GetStreamAsync(uri);
+            var response = await _client.GetAsync(uri);
+            var stream = await response.Content.ReadAsStreamAsync();
+
+            triviaQuizzes = await JsonSerializer.DeserializeAsync<List<TriviaModel>>(stream);
+            Console.WriteLine(triviaQuizzes);
+            return triviaQuizzes;
+        }
     }
-    
 }

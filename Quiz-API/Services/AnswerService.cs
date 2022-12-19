bd996a9b35bdc4ab0120d3aa64a10d5bb59c30fa
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-19 15:02:09 +0100

    3

--- Quiz-API/Services/AnswerService.cs
+++ Quiz-API/Services/AnswerService.cs
@@ -2,57 +2,58 @@
 using Quiz_API.Models;
 using Quiz_API.Persistance;
 
-namespace Quiz_API.Services;
-
-public class AnswerService
+namespace Quiz_API.Services
 {
-    private AnswerAdapter _adapter;
 
-    public AnswerService(AnswerAdapter adapter)
+    public class AnswerService
     {
-        _adapter = adapter;
-    }
+        private AnswerAdapter _adapter;
 
-
-    public List<Answer> GetAllAnswers()
-    {
-        return _adapter.GetAllAnswers();
-    }
-
-    public List<Answer> GetAnswerByID(Guid id)
-    {
-        //List<Answer> listOfAnswers;
-        return _adapter.GetAllAnswers().Where(answer => answer.QuestionId == id).ToList();
-        //return listOfAnswers;
-    }
-
-    public Answer PostAnswer(Answer answer)
-    {
-        _adapter.SaveNewAnswer(answer);
-        return (answer);
-    }
-
-    public bool PutAnswer(Answer answer)
-    {
-        var foundAnswer = _adapter.GetAllAnswers().Where(x => x.Id == answer.Id).FirstOrDefault();
-        if (foundAnswer == null)
+        public AnswerService(AnswerAdapter adapter)
         {
-            return false;
+            _adapter = adapter;
         }
-        _adapter.DeleteAnswer(foundAnswer);
-        _adapter.SaveNewAnswer(answer);
-        return true;
-    }
 
-    public bool DeleteAnswer(Guid id)
-    {
-        var foundAnswer = _adapter.GetAllAnswers().Where(x => x.Id == id).FirstOrDefault();
-        if (foundAnswer == null)
+
+        public List<Answer> GetAllAnswers()
         {
-            return false;
+            return _adapter.GetAllAnswers();
         }
-        _adapter.DeleteAnswer(foundAnswer);
-        return true;
-    }
 
+        public List<Answer> GetAnswerByID(Guid id)
+        {
+            //List<Answer> listOfAnswers;
+            return _adapter.GetAllAnswers().Where(answer => answer.QuestionId == id).ToList();
+            //return listOfAnswers;
+        }
+
+        public Answer PostAnswer(Answer answer)
+        {
+            _adapter.SaveNewAnswer(answer);
+            return (answer);
+        }
+
+        public bool PutAnswer(Answer answer)
+        {
+            var foundAnswer = _adapter.GetAllAnswers().Where(x => x.Id == answer.Id).FirstOrDefault();
+            if (foundAnswer == null)
+            {
+                return false;
+            }
+            _adapter.DeleteAnswer(foundAnswer);
+            _adapter.SaveNewAnswer(answer);
+            return true;
+        }
+
+        public bool DeleteAnswer(Guid id)
+        {
+            var foundAnswer = _adapter.GetAllAnswers().Where(x => x.Id == id).FirstOrDefault();
+            if (foundAnswer == null)
+            {
+                return false;
+            }
+            _adapter.DeleteAnswer(foundAnswer);
+            return true;
+        }
+    }
 }

6682e8e6aaba184f84d4f15ce2ed64de50fff8bc
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-19 14:39:52 +0100

    TriviaAdapter

--- Quiz-API/Adapters/TriviaAdapter.cs
+++ Quiz-API/Adapters/TriviaAdapter.cs
@@ -13,26 +13,26 @@ public class TriviaAdapter
         _triviaRepository = new TriviaRepository();
     }
 
+
     public async Task<QuizModel> GetOneTriviaQuiz()
     {
         var response = await _triviaRepository.GetTriviaAsync();
-        
-        QuizModel responseQuiz = null;
-        
-         foreach (TriviaModel quiz in response)
-         {  
-             List<Answer> answers = new List<Answer>();
-             responseQuiz = new QuizModel(quiz.Category, quiz.Id, answers, quiz.Question);
-             
-             responseQuiz.Answers.Add(new Answer(quiz.CorrectAnswer, responseQuiz.Id, true));
-             
-             foreach (string answer in quiz.IncorrectAnswers)
-             {
-                 responseQuiz.Answers.Add(new Answer(answer, responseQuiz.Id, false));
-             }
-         }
 
-         return responseQuiz;
+        QuizModel responseQuiz = null;
+
+        foreach (TriviaModel quiz in response)
+        {
+            List<Answer> answers = new List<Answer>();
+            responseQuiz = new QuizModel(quiz.Category, quiz.Id, answers, quiz.Question);
+            responseQuiz.Answers.Add(new Answer(quiz.CorrectAnswer, responseQuiz.Id, true));
+
+            foreach (string answer in quiz.IncorrectAnswers)
+            {
+                responseQuiz.Answers.Add(new Answer(answer, responseQuiz.Id, false));
+            }
+        }
+
+        return responseQuiz;
     }
 
 }

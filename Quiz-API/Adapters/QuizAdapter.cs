3bddb6975f3cf52fb96aa2269d49097201297da1
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-19 14:39:17 +0100

    QuizAdapter

--- Quiz-API/Adapters/QuizAdapter.cs
+++ Quiz-API/Adapters/QuizAdapter.cs
@@ -19,8 +19,8 @@ public class QuizAdapter : IQuizAdapter
         _questionRepository = questionRepository;
         _answerRepository = answerRepository;
         _triviaAdapter = triviaAdapter;
-
     }
+
 
     // Questions:
     public List<Question> GetAllQuestions()
@@ -79,42 +79,21 @@ public class QuizAdapter : IQuizAdapter
     public QuizModel? GetRandomQuizFromDb()
     {
         Question question;
-        //List<Answer> answers;
         QuizModel quiz;
         var questions = _questionRepository.Get();
         if (questions.Count() > 0)
         {
-            Console.WriteLine($"_________questions.Count(): {questions.Count()}");
             var random = new Random();
             int index = random.Next(questions.Count);
-            Console.WriteLine($"_________index: {index}");
-
             question = questions[index];
-
             var answers = _answerRepository.Get().Where(x => x.QuestionId == question.Id).ToList();
-
             quiz = new QuizModel(question.Category, question.TriviaId, answers, question.Text);
-
-            //foreach (var answer in answers)
-            //    {
-
-            //    }
 
             return quiz;
         }
         return null;
     }
 
-
-    //public async Task<QuizModel> GetQuizFromTrivia()
-    //{
-    //    return (await _triviaAdapter.GetOneTriviaQuiz());
-    //}
-
-    //public async Task<IActionResult> GetTrivia()
-    //{
-    //    return Ok(await _quizService.GetTriviaQuiz());
-    //}
 
     public async Task<QuizModel> GetTriviaQuiz()
     {
@@ -124,53 +103,14 @@ public class QuizAdapter : IQuizAdapter
     // First Get in QuizController:
     public async Task<QuizModel> GetQuiz()
     {
-        //var triviaQuiz = GetQuizFromTrivia();
         var dbQuiz = GetRandomQuizFromDb();
         var random = new Random();
         int source = random.Next(2); // 2 sources: Trivia and DB.
-        Console.WriteLine($"__________source: {source}");
-
-        //Console.WriteLine($"__________triviaQuiz.GetType: {triviaQuiz.GetType()}");
-
-
-        //if (source == 0)
-        //{
-        //    return await GetQuizFromTrivia();
-        //}
-        //return GetRandomQuizFromDb();
 
         return (await _triviaAdapter.GetOneTriviaQuiz());
-
-        //return  dbQuiz;
     }
 
 
-
-    // Skall det bara finnas en generell get metod som anropas på olika sätt ifrån service? 
-
-    // Skall ta emot ett ID och skicka tillbaka en quizmodel med rätt ID
-    //public QuizModel GetQuiz(Guid id)
-    //{
-    //    Question question = _questionRepository.Get(id);
-    //    List<Answer> listOfAnswers = _answerRepository.GetAnswerById(id);
-
-    //    QuizModel responseQuiz = new QuizModel(question.Category, question.TriviaId, listOfAnswers, question.Text);
-
-    //    return responseQuiz;
-    //}
-
-    //public QuizModel GetOneRandomQuiz()
-    //{
-    //    List<Question> Questions = _questionRepository.Get();
-    //    var random = new Random();
-    //    int index = random.Next(Questions.Count)-1;
-    //    var chosenQuestion = Questions[index];
-
-    //    List<Answer> Answers = _answerRepository.GetAnswers(chosenQuestion.Id);
-
-    //    QuizModel responseQuiz = new QuizModel(chosenQuestion.Category, chosenQuestion.TriviaId, Answers, chosenQuestion.Text);
-    //    return responseQuiz;
-    //}
 
     // Finns quizzen i vår databas? I Service? 
     public bool DoesQuizExist(Guid id)
@@ -179,24 +119,23 @@ public class QuizAdapter : IQuizAdapter
         {
             return true;
         }
-
         return false;
     }
+
 
     public void Post(QuizModel quiz)
     {
         Question question = new Question(quiz.Question, quiz.TriviaId, quiz.Category);
         _questionRepository.Post(question);
 
-        Console.WriteLine($"Post answer question: {question.Text} Id: {question.Id}");
         foreach (Answer answer in quiz.Answers)
         {
             // Need to set QuestionId here, since question get's a fresh Id i constructor above.
             answer.QuestionId = question.Id;
-            Console.WriteLine($"Post answer answer: {answer.AnswerText} questionID: {answer.QuestionId}");
             _answerRepository.Post(answer);
         }
     }
+
 
     public void Put(QuizModel quiz)
     {
@@ -208,6 +147,8 @@ public class QuizAdapter : IQuizAdapter
             _answerRepository.Put(answer);
         }
     }
+
+
     public void Delete(QuizModel quiz)
     {
         Question question = new Question(quiz.Question, quiz.TriviaId, quiz.Category);
@@ -218,7 +159,5 @@ public class QuizAdapter : IQuizAdapter
             _answerRepository.Delete(answer);
         }
     }
-
-
 
 }

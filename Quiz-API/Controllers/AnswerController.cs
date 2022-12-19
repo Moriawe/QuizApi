79e1d68310cf49298b03633487f104ace5a1d8a8
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-19 15:01:43 +0100

    2

--- Quiz-API/Controllers/AnswerController.cs
+++ Quiz-API/Controllers/AnswerController.cs
@@ -5,80 +5,81 @@ using Quiz_API.Models;
 using Quiz_API.Services;
 using Swashbuckle.AspNetCore.Annotations;
 
-namespace Quiz_API.Controllers;
-
-[ApiController]
-[Route("api/[controller]")]
-[Produces(MediaTypeNames.Application.Json)]
-public class AnswerController : ControllerBase
+namespace Quiz_API.Controllers
 {
-    private AnswerService _service;
 
-    public AnswerController(AnswerService service)
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class AnswerController : ControllerBase
     {
-        _service = service;
-    }
+        private AnswerService _service;
 
-
-    [HttpGet]
-    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
-    public IActionResult Get()
-    {
-        return Ok(_service.GetAllAnswers());
-    }
-
-
-    // GET api/values/5
-    [HttpGet("{id}")]
-    [SwaggerResponse((int)HttpStatusCode.NotFound)]
-    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Answer))]
-    public IActionResult Get(Guid id)
-    {
-        return Ok(_service.GetAnswerByID(id));
-    }
-
-
-    // POST api/values
-    [HttpPost]
-    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Answer))]
-    public IActionResult Post(Answer answer) // Probably skip [FromBody]
-    {
-        return Ok(_service.PostAnswer(answer));
-    }
-
-
-    // PUT api/values/5
-    [HttpPut("{id}")]
-    [SwaggerResponse((int)HttpStatusCode.NotFound)]
-    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
-    public IActionResult Put(Guid id, [FromBody] Answer answer)
-    {
-        if (id != answer.Id)
+        public AnswerController(AnswerService service)
         {
-            return BadRequest("Invalid id");
+            _service = service;
         }
 
-        var isAnswerUpdated = _service.PutAnswer(answer);
-        if (isAnswerUpdated)
+
+        [HttpGet]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
+        public IActionResult Get()
         {
-            return Ok();
+            return Ok(_service.GetAllAnswers());
         }
-        return NotFound();
-    }
 
 
-    // DELETE api/values/5
-    [HttpDelete("{id}")]
-    [SwaggerResponse((int)HttpStatusCode.NotFound)]
-    [SwaggerResponse((int)HttpStatusCode.NoContent)]
-    public IActionResult Delete(Guid id)
-    {
-        var isAnswerDeleted = _service.DeleteAnswer(id);
-        if (isAnswerDeleted)
+        // GET api/values/5
+        [HttpGet("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Answer))]
+        public IActionResult Get(Guid id)
         {
-            return Ok();
+            return Ok(_service.GetAnswerByID(id));
         }
-        return NotFound();
-    }
 
+
+        // POST api/values
+        [HttpPost]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Answer))]
+        public IActionResult Post(Answer answer) // Probably skip [FromBody]
+        {
+            return Ok(_service.PostAnswer(answer));
+        }
+
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<Answer>))]
+        public IActionResult Put(Guid id, [FromBody] Answer answer)
+        {
+            if (id != answer.Id)
+            {
+                return BadRequest("Invalid id");
+            }
+
+            var isAnswerUpdated = _service.PutAnswer(answer);
+            if (isAnswerUpdated)
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        [SwaggerResponse((int)HttpStatusCode.NotFound)]
+        [SwaggerResponse((int)HttpStatusCode.NoContent)]
+        public IActionResult Delete(Guid id)
+        {
+            var isAnswerDeleted = _service.DeleteAnswer(id);
+            if (isAnswerDeleted)
+            {
+                return Ok();
+            }
+            return NotFound();
+        }
+    }
 }

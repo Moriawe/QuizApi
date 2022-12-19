c05770085a27e6735d977e07127d7b494b6029c5
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-19 14:33:57 +0100

    QuizController

--- Quiz-API/Controllers/QuizController.cs
+++ Quiz-API/Controllers/QuizController.cs
@@ -14,13 +14,13 @@ namespace Quiz_API.Controllers;
 [Produces(MediaTypeNames.Application.Json)]
 public class QuizController : ControllerBase
 {
-        private IQuizService _quizService;
-        private TriviaRepository _triviaRepository = new();
-        
-        public QuizController(IQuizService quizService)
-        {
+    private IQuizService _quizService;
+    private TriviaRepository _triviaRepository = new();
+
+    public QuizController(IQuizService quizService)
+    {
         _quizService = quizService;
-        }
+    }
 
 
     [EnableCors]
@@ -31,52 +31,41 @@ public class QuizController : ControllerBase
         return Ok(await _quizService.GetQuiz());
     }
 
-    //[SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizModel))]
-    //public async Task<IActionResult> GetTrivia()
-    //{
-    //    return Ok(await _quizService.GetTriviaQuiz());
-    //}
 
     // GET: api/values
     [HttpGet("Database")]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizModel))]
-         public IActionResult GetDatabase()
-        {
-            return Ok(_quizService.GetDbQuiz());
-        } 
-        
-        // GET: api/values
-        [HttpGet("TriviaRepo")]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(TriviaModel))]
-        public async Task<IActionResult> GetTriviaModel()
-        {
-            return Ok(await _triviaRepository.GetTriviaAsync());
-            //return Ok(await _quizService.GetTriviaQuiz());
-        }
-        
-        // GET: api/values
-        [HttpGet("Trivia")]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizModel))]
-        public async Task<IActionResult> GetTrivia()
-        {
-            return Ok(await _quizService.GetTriviaQuiz());
-        }
-        
-        // POST api/values
-        [HttpPost]
-        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<QuizModel>))]
-        public IActionResult Post([FromBody] QuizModel quizModel)
-        {
-            return Ok(_quizService.AddQuizToDatabase(quizModel));
-        }
-     
-        // // GET api/values/5
-        // [HttpGet("{limit}")]
-        // [SwaggerResponse((int)HttpStatusCode.NotFound)]
-        // [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(Answer))]
-        // public IActionResult Get(int limit)
-        // {
-        //    return Ok(_quizService);
-        // }
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizModel))]
+    public IActionResult GetDatabase()
+    {
+        return Ok(_quizService.GetDbQuiz());
+    }
+
+
+    // GET: api/values
+    [HttpGet("TriviaRepo")]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(TriviaModel))]
+    public async Task<IActionResult> GetTriviaModel()
+    {
+        return Ok(await _triviaRepository.GetTriviaAsync());
+        //return Ok(await _quizService.GetTriviaQuiz());
+    }
+
+
+    // GET: api/values
+    [HttpGet("Trivia")]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizModel))]
+    public async Task<IActionResult> GetTrivia()
+    {
+        return Ok(await _quizService.GetTriviaQuiz());
+    }
+
+
+    // POST api/values
+    [HttpPost]
+    [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(List<QuizModel>))]
+    public IActionResult Post([FromBody] QuizModel quizModel)
+    {
+        return Ok(_quizService.AddQuizToDatabase(quizModel));
+    }
 
 }

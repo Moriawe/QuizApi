41d2abc0d96b44d2bd748fbab316c6cea36c30e9
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-19 15:43:12 +0100

    IAnswerRepository

--- Quiz-API/Interfaces/Repositories/IAnswerRepository.cs
+++ Quiz-API/Interfaces/Repositories/IAnswerRepository.cs
@@ -6,24 +6,11 @@ namespace Quiz_API
 {
 	public interface IAnswerRepository
 	{
-        //public List<Answer> Get();
-        //public Answer? Get(Guid Id);
-        //public List<Answer> GetAnswers(Guid questionId);
-        //public Answer Post(Answer answer);
-        //public Answer? Put(Answer answer);
-        //public bool Delete(Answer answer);
-
-
         public List<Answer> Get();
-
         public Answer? Get(Guid Id);
-
         public List<Answer> GetAnswerByQuestionId(Guid questionId);
-
         public Answer Post(Answer answer);
-
         public Answer? Put(Answer answer);
-
         public bool Delete(Answer answer);
     }
 }

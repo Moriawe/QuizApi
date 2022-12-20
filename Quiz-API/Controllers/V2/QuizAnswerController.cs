13ea4534ee9e5b560bfdb755c5608f10569b6fa8
krdevel <91143280+krdevel@users.noreply.github.com>
2022-12-20 08:31:12 +0100

    Works with     [Route("api/v2/[controller]")] , but not I swagger explorer (upper right dropdown)

---
+++ Quiz-API/Controllers/V2/QuizAnswerController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Quiz_API.Models;
+using Quiz_API.Services;
+using Swashbuckle.AspNetCore.Annotations;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Quiz_API.Controllers.V2
+{
+    [ApiController]
+    [Route("api/v2/[controller]")]
+    [Produces(MediaTypeNames.Application.Json)]
+    public class QuizAnswerController : ControllerBase
+    {
+        private IQuizService _quizService;
+
+        public QuizAnswerController(IQuizService quizService)
+        {
+            _quizService = quizService;
+        }
+
+
+        // POST api/values
+        [EnableCors]
+        [HttpPost]
+        [SwaggerResponse((int)HttpStatusCode.OK, Type = typeof(QuizSolution))]
+        public IActionResult PostAnswer(QuizAnswer quizAnswer)
+        {
+            return Ok(_quizService.EvaluateQuizAnswer(quizAnswer));
+        }
+    }
+
+}
+
